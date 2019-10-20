1dac77b09314b2d8955606b909112b4de34df59c
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-10-21 02:11:22 +0300

    bug

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -182,7 +182,7 @@ static int hyundai_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
         OP_LKAS_live -= 1;
       }
     } 
-  } eles {
+  } else {
     if (bus_num == 0) {
       bus_fwd = 1;
     }
