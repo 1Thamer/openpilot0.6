2dc1041648df35aaf4a0abf9103efee372702a88
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-12-03 15:25:28 +0300

    bug

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -1,4 +1,5 @@
 from cereal import car
+from common.numpy_fast import clip
 from selfdrive.car import apply_std_steer_torque_limits
 from selfdrive.car.hyundai.hyundaican import create_lkas11, create_lkas12, \
                                              create_1191, create_1156, \
